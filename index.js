dd3ba5f8844c1a178ae750b4947813a5f49ef8fd
gy <372301467@qq.com>
2021-06-18 14:31:16 +0800

    添加 github 链接 复制连接后的提醒 修复 bug：导致字符串中的 </script>…… 添加 readme.md

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ var $html = document.getElementById('html')
 var $css = document.getElementById('css')
 var $js = document.getElementById('js')
 
-var $iframe = document.getElementsByClassName('show')
-
 function run() {
   sync($html.value, $css.value, $js.value)
 }
 
 function sync(html, css, js) {
-  $iframe[0].srcdoc = `
+  document.querySelector('.show').srcdoc = `
     <html>
       <head>
         <style>${css}</style>
       </head>
       <body>
         ${html}
-        <script>${js}</script>
+        <script type="text/javascript">${js}<\/script>
       </body>
     </html>
   `
@@ -24,22 +22,23 @@ function sync(html, css, js) {
 
 function share() {
   navigator.clipboard.writeText(
-    location.href.split('?')[0]
-      + `?html=${escape($html.value)
+    location.href.split('?')[0] +
+      `?html=${escape($html.value)
       }&css=${escape($css.value)
       }&js=${escape($js.value)}`
   )
+  alert('链接已复制到剪切板')
 }
 
 
 // 读取分享的内容
 let search = location.search
 search = search.split('&js=')
-let js = unescape(search[1])
+let js = unescape(search[1] || '')
 search = search[0].split('&css=')
-let css = unescape(search[1])
+let css = unescape(search[1] || '')
 search = search[0].split('?html=')
-let html = unescape(search[1])
+let html = unescape(search[1] || '')
 if(html || css || js) {
   $html.value = html
   $css.value = css
---
+++ readme.md
@@ -0,0 +1,9 @@
+# codeshow
+用来展示和分享代码  
+[试一下](https://dagaiguanyu.github.io/showcode/index.html)
+
+# 功能与特点
++ 在网页上写代码，在网页上看代码
++ 分享网页代码
++ 超轻量级（html、css、js 加起来不超过 100 行）
++ 没有后端，不保存代码（你写的代码被放入 url 里，如果你的代码很长，url 自然也会很长）
