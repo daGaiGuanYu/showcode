5a96ed4e447ad1fbe4f202b9eb5bd42a8efb8ca4
gy <372301467@qq.com>
2021-06-18 12:50:41 +0800

    分享

--- index.js
+++ index.js
@@ -5,19 +5,44 @@ var $js = document.getElementById('js')
 var $iframe = document.getElementsByClassName('show')
 
 function run() {
-  $iframe[0].srcdoc = tmpl($html.value, $css.value, $js.value)
+  sync($html.value, $css.value, $js.value)
 }
 
-function tmpl(html, css, js) {
-  return `
-  <html>
-    <head>
-      <style>${css}</style>
-    </head>
-    <body>
-      ${html}
-      <script>${js}</script>
-    </body>
-  </html>
+function sync(html, css, js) {
+  $iframe[0].srcdoc = `
+    <html>
+      <head>
+        <style>${css}</style>
+      </head>
+      <body>
+        ${html}
+        <script>${js}</script>
+      </body>
+    </html>
   `
+}
+
+function share() {
+  navigator.clipboard.writeText(
+    location.href.split('?')[0]
+      + `?html=${escape($html.value)
+      }&css=${escape($css.value)
+      }&js=${escape($js.value)}`
+  )
+}
+
+
+// 读取分享的内容
+let search = location.search
+search = search.split('&js=')
+let js = unescape(search[1])
+search = search[0].split('&css=')
+let css = unescape(search[1])
+search = search[0].split('?html=')
+let html = unescape(search[1])
+if(html || css || js) {
+  $html.value = html
+  $css.value = css
+  $js.value = js
+  sync(html, css, js)
 }
